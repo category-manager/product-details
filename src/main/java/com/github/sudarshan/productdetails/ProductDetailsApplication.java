e39a7e1b20a01a9f8e3ccbd0e8b3650a0b07f0d2
sudarshan bhat <sudarshanrbhat.srb2@gmail.com>
2023-10-15 16:37:12 +0530

    initialize

---
+++ src/main/java/com/github/sudarshan/productdetails/ProductDetailsApplication.java
@@ -0,0 +1,14 @@
+package com.github.sudarshan.productdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDetailsApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/github/sudarshan/productdetails/ProductDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sudarshan.productdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
